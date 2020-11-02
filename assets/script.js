e310b37308a27b2a14d226205daf84824ea3d3bb
Greg Stead <gregstead@gmail.com>
2020-11-01 22:05:12 -0600

    Added functions for burger animation

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+
+// For hamburger 
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    // Toggle nav
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        // animate links in 
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
+            };
+        });
+
+        // Burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
