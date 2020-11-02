b6ca84de2722f82f19fcc61143fefb7d6058539b
Greg Stead <gregstead@gmail.com>
2020-11-02 00:01:48 -0600

    tweaked mobile responsiveness of hamburger/nav links

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,11 @@
 
-// For hamburger 
+// Hamburger and Nav Links animation handler
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
 
     // Toggle nav
-
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
 
