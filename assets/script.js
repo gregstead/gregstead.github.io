2cb4b39258703c6ea68654b2f546eb917c476bdc
Greg Stead <gregstead@gmail.com>
2020-11-02 19:23:37 -0600

    Started render functions for page elements

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,74 @@
 
 // Hamburger and Nav Links animation handler
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
 
-    // Toggle nav
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
 
-        // animate links in 
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
-            };
-        });
+// Click event handler
+document.addEventListener('click', (event) => {
+    // Prevent page from redirecting
+    event.preventDefault();
 
-        // Burger animation
-        burger.classList.toggle('toggle');
+    if (event.target.matches('.burger') || event.target.matches('.line')) {
+        burgerAnimation();
+    };
+
+    if (event.target.matches('#HomeLi')) {
+        renderHomeEl();
+    };
+
+    if (event.target.matches('#aboutLi')) {
+        renderAboutEl();
+    };
+
+    if (event.target.matches('#projectsLi')) {
+        renderProjectsEl();
+    };
+
+    if (event.target.matches('#resumeLi')) {
+        renderResumeEl();
+    };
+
+    if (event.target.matches('#contactLi')) {
+        renderContactEl();
+    };
+
+});
+
+function burgerAnimation() {
+    nav.classList.toggle('nav-active');
+
+    // animate links in 
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 6 + .5}s`;
+        };
     });
-}
 
-navSlide();
+    // Burger animation
+    burger.classList.toggle('toggle');
+};
+
+function renderHomeEl() {
+    console.log("Home element");
+};
+
+function renderAboutEl() {
+    console.log("About element");
+};
+
+function renderProjectsEl() {
+    console.log("Projects element");
+};
+
+function renderResumeEl() {
+    console.log("Resume element");
+};
+
+function renderContactEl() {
+    console.log("Contact element");
+};
+
