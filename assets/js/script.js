f6fc24224588ac335a29f74be9afb70390d6aa08
Greg Stead <gregstead@gmail.com>
2021-03-09 12:09:21 -0600

    navbar translates on burger click

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,22 @@ function ready(fn) {
   }
 }
 
+function toggleClass(node, className) {
+  node.classList.toggle(className);
+}
+
 function burgerClickHandler(burger) {
-  burger.classList.toggle("active");
-  for (i = 0; i < 3; i++) {
-    burger.children[i].classList.toggle("active");
+  // Menu selector
+  const menu = burger.parentNode.querySelector(".nav-links");
+  toggleClass(menu, "nav-active");
+
+  // Toggle class of burger lines for animation
+  const lines = burger.children;
+  for (i = 0; i < lines.length; i++) {
+    toggleClass(burger.children[i], "active");
   }
+  // Toggle class of burger for animation
+  toggleClass(burger, "active");
 }
 
 ready(async () => {
