589eeb6ac95c59750294b0913e25df1ba5191346
Greg Stead <gregstead@gmail.com>
2021-03-09 18:26:58 -0600

    factors css into modules

--- views/components/Footer/index.js
+++ views/components/Footer/index.js
@@ -1,8 +1,14 @@
+// Link CSS module
+const styleSrc = "./views/components/Footer/style.css";
+document.head.innerHTML += `<link type="text/css" rel="stylesheet" href="${styleSrc}"> `;
+
 let Footer = {
   render: async () => {
     let view = `
-        
-      `;
+      <footer>
+        <div></div>
+      </footer>
+    `;
     return view;
   },
   after_render: async () => {},
