4888a9ef987523685bf294fbf88bad94e856b800
Greg Stead <gregstead@gmail.com>
2021-03-20 18:10:34 -0500

    updating style

--- app.js
+++ app.js
@@ -9,17 +9,6 @@ import Error404 from "./views/pages/Error404/index.js";
 import Navbar from "./views/components/Navbar/index.js";
 import Bottombar from "./views/components/Footer/index.js";
 
-import Utils from "./services/utils/index.js";
-
-// List of supported routes. Any url other than these routes will throw a 404 error
-const routes = {
-  "/": About,
-  "/home": Home,
-  "/about": About,
-  "/projects": Projects,
-  "/resume": Resume,
-};
-
 // The router code. Takes a URL, checks against the list of supported routes and then renders the corresponding content page.
 const router = async () => {
   // Lazy load view element:
@@ -30,23 +19,13 @@ const router = async () => {
   // Render the Header and footer of the page
   header.innerHTML = await Navbar.render();
   await Navbar.after_render();
+  const content_data =
+    (await About.render()) +
+    (await Projects.render()) +
+    (await Resume.render());
+  content.innerHTML = content_data;
   footer.innerHTML = await Bottombar.render();
   await Bottombar.after_render();
-
-  // Get the parsed URl from the addressbar
-  let request = Utils.parseRequestURL();
-
-  // Parse the URL and if it has an id part, change it with the string ":id"
-  let parsedURL =
-    (request.resource ? "/" + request.resource : "/") +
-    (request.id ? "/:id" : "") +
-    (request.verb ? "/" + request.verb : "");
-
-  // Get the page from our hash of supported routes.
-  // If the parsed URL is not in our list of supported routes, select the 404 page instead
-  let page = routes[parsedURL] ? routes[parsedURL] : Error404;
-  content.innerHTML = await page.render();
-  await page.after_render();
 };
 
 // Listen on hash change:
